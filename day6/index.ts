832bcce98995e185dcd2eb12d7d70d4cdb402480
rstagi <r.stagi96@gmail.com>
2023-12-06 06:38:08 +0100

    feat: solve day6

--- day6/index.ts
+++ day6/index.ts
@@ -1,7 +1,58 @@
+import { mul } from "../utils/reducers";
+
 /* CHALLENGE 1 */
-function solve1(input: string[]) {}
+function solve1(input: string[]) {
+  const { times, distances } = readInput(input);
+
+  const totWays = [];
+  for (let i = 0; i < times.length; ++i) {
+    totWays.push(calcTotalWays(times[i], distances[i]));
+  }
+
+  return totWays.reduce(...mul);
+}
 
 /* CHALLENGE 2 */
-function solve2(input: string[]) {}
+function solve2(input: string[]) {
+  const { times, distances } = readInput(input);
+
+  let ways = calcTotalWays(
+    parseInt(times.join("")),
+    parseInt(distances.join("")),
+  );
+
+  return ways;
+}
+
+/* SHARED */
+function readInput(input: string[]) {
+  return {
+    times: input[0]
+      .split(": ")[1]
+      .trim()
+      .split(" ")
+      .map((n) => parseInt(n))
+      .filter((n) => !isNaN(n)),
+    distances: input[1]
+      .split(": ")[1]
+      .trim()
+      .split(" ")
+      .map((n) => parseInt(n))
+      .filter((n) => !isNaN(n)),
+  };
+}
+
+function calcTotalWays(timeAvailable: number, distance: number) {
+  let ways = 0;
+  for (let pressFor = 1; pressFor < timeAvailable; ++pressFor) {
+    // pressFor = velocity in mm/ms
+    // timeAvailable - pressFor = remaining time in ms
+    // pressFor * (timeAvailable - pressFor) = distance in mm
+    if (pressFor * (timeAvailable - pressFor) > distance) {
+      ways += 1;
+    }
+  }
+  return ways;
+}
 
 module.exports = { solve1, solve2 };
---
+++ utils/reducers.ts
@@ -0,0 +1,2 @@
+export const sum = [(sum: number, n: number) => sum + n, 0] as const;
+export const mul = [(mul: number, n: number) => mul * n, 1] as const;
