e2820baf4ecfb66bfe05eafd76cdaf17f9a6d7e6
rstagi <r.stagi96@gmail.com>
2023-12-07 07:30:59 +0100

    feat: solve day7 and refactor

--- day7/index.ts
+++ day7/index.ts
@@ -1,21 +1,90 @@
 import * as __input from "../utils/input";
 import * as __reducers from "../utils/reducers";
-type HandInfo = [string, number];
+import * as __sorters from "../utils/sorters";
 
 /* CHALLENGE 1 */
 export function solve1(input: string[]) {
-  const parsedInput = parseInput(input);
-  // console.log(parsedInput);
+  return solve(parseInput(input), { considerJokers: false });
+}
 
-  const five: HandInfo[] = [],
-    four: HandInfo[] = [],
+/* CHALLENGE 2 */
+export function solve2(input: string[]) {
+  return solve(parseInput(input), { considerJokers: true });
+}
+
+/* SHARED */
+// Parse the input into a usable format. An example of input is provided below in the __forceInput.input variable.
+function parseInput(input: string[]): HandInfo[] {
+  return input
+    .map((line) => line.split(" "))
+    .filter((line) => line.length > 1)
+    .map(([hand, bid]) => [hand, parseInt(bid)]);
+}
+
+function solve(
+  hands: HandInfo[],
+  { considerJokers }: { considerJokers: boolean },
+): number {
+  const {
+    fiveOfKind,
+    fourOfKind,
+    fullHouse,
+    threeOfKind,
+    twoPairs,
+    onePair,
+    highCard,
+  } = groupByHandType(hands, { considerJokers });
+
+  const CARD_TO_POINT = {
+    "2": 2,
+    "3": 3,
+    "4": 4,
+    "5": 5,
+    "6": 6,
+    "7": 7,
+    "8": 8,
+    "9": 9,
+    T: 10,
+    J: 11,
+    Q: 12,
+    K: 13,
+    A: 14,
+  };
+
+  const CARD_TO_POINT_WITH_JOKER = {
+    ...CARD_TO_POINT,
+    J: 1,
+  };
+
+  const scale = considerJokers ? CARD_TO_POINT_WITH_JOKER : CARD_TO_POINT;
+  const sortedHandsLowestToHighest = [
+    ...highCard.sort(sortByWeakestHand(scale)),
+    ...onePair.sort(sortByWeakestHand(scale)),
+    ...twoPairs.sort(sortByWeakestHand(scale)),
+    ...threeOfKind.sort(sortByWeakestHand(scale)),
+    ...fullHouse.sort(sortByWeakestHand(scale)),
+    ...fourOfKind.sort(sortByWeakestHand(scale)),
+    ...fiveOfKind.sort(sortByWeakestHand(scale)),
+  ];
+
+  return sortedHandsLowestToHighest.reduce((tot, [_, bid], pos) => {
+    return tot + bid * (pos + 1);
+  }, 0);
+}
+
+type HandInfo = [string, number];
+function groupByHandType(
+  hands: HandInfo[],
+  { considerJokers }: { considerJokers: boolean },
+) {
+  const fiveOfKind: HandInfo[] = [],
+    fourOfKind: HandInfo[] = [],
     fullHouse: HandInfo[] = [],
-    three: HandInfo[] = [],
-    two: HandInfo[] = [],
-    one: HandInfo[] = [],
+    threeOfKind: HandInfo[] = [],
+    twoPairs: HandInfo[] = [],
+    onePair: HandInfo[] = [],
     highCard: HandInfo[] = [];
-  for (const [hand, _bid] of parsedInput) {
-    const bid = parseInt(_bid);
+  for (const [hand, bid] of hands) {
     const handInfo = hand.split("").reduce(
       (acc, c) => {
         acc[c] = (acc[c] || 0) + 1;
@@ -24,28 +93,33 @@ export function solve1(input: string[]) {
       {} as Record<string, number>,
     );
 
-    const sortedPairs = Object.entries(handInfo).sort((a, b) => b[1] - a[1]);
-    // console.log(sortedPairs);
+    const sortedHandInfo = Object.entries(handInfo)
+      .sort((a, b) => __sorters.desc(a[1], b[1]))
+      .map(([card, occurrences]) => ({ card, occurrences }));
 
-    switch (sortedPairs[0][1]) {
+    const bestHandInfo = considerJokers
+      ? improveHandWithJoker(sortedHandInfo)
+      : sortedHandInfo;
+
+    switch (bestHandInfo[0].occurrences) {
       case 5:
-        five.push([hand, bid]);
+        fiveOfKind.push([hand, bid]);
         break;
       case 4:
-        four.push([hand, bid]);
+        fourOfKind.push([hand, bid]);
         break;
       case 3:
-        if (sortedPairs[1][1] === 2) {
+        if (bestHandInfo[1].occurrences === 2) {
           fullHouse.push([hand, bid]);
         } else {
-          three.push([hand, bid]);
+          threeOfKind.push([hand, bid]);
         }
         break;
       case 2:
-        if (sortedPairs[1][1] === 2) {
-          two.push([hand, bid]);
+        if (bestHandInfo[1].occurrences === 2) {
+          twoPairs.push([hand, bid]);
         } else {
-          one.push([hand, bid]);
+          onePair.push([hand, bid]);
         }
         break;
       case 1:
@@ -54,99 +128,42 @@ export function solve1(input: string[]) {
     }
   }
 
-  // console.log(five, four, fullHouse, three, two, one, highCard);
-
-  five.sort((a, b) => compareHands(a[0], b[0]));
-  four.sort((a, b) => compareHands(a[0], b[0]));
-  fullHouse.sort((a, b) => compareHands(a[0], b[0]));
-  three.sort((a, b) => compareHands(a[0], b[0]));
-  two.sort((a, b) => compareHands(a[0], b[0]));
-  one.sort((a, b) => compareHands(a[0], b[0]));
-  highCard.sort((a, b) => compareHands(a[0], b[0]));
-
-  // console.log(five, four, fullHouse, three, two, one, highCard);
-
-  console.log(
-    [
-      ["5872T", 522],
-      ["95J8T", 666],
-      ["6Q8T7", 944],
-    ].sort((a, b) => compareHands(a[0] as string, b[0] as string)),
-  );
-
-  let total = 0,
-    pos = parsedInput.length;
-  console.log(pos);
-  for (const hand of five) {
-    total += hand[1] * pos;
-    pos -= 1;
-  }
-  for (const hand of four) {
-    total += hand[1] * pos;
-    pos -= 1;
-  }
-  for (const hand of fullHouse) {
-    total += hand[1] * pos;
-    pos -= 1;
-  }
-  for (const hand of three) {
-    total += hand[1] * pos;
-    pos -= 1;
-  }
-  for (const hand of two) {
-    total += hand[1] * pos;
-    pos -= 1;
-  }
-  for (const hand of one) {
-    total += hand[1] * pos;
-    pos -= 1;
-  }
-  for (const hand of highCard) {
-    total += hand[1] * pos;
-    pos -= 1;
-  }
-
-  console.log(pos);
-  return total;
+  return {
+    fiveOfKind,
+    fourOfKind,
+    fullHouse,
+    threeOfKind,
+    twoPairs,
+    onePair,
+    highCard,
+  };
 }
 
-const scale = {
-  "2": 2,
-  "3": 3,
-  "4": 4,
-  "5": 5,
-  "6": 6,
-  "7": 7,
-  "8": 8,
-  "9": 9,
-  T: 10,
-  J: 11,
-  Q: 12,
-  K: 13,
-  A: 14,
-};
-function compareHands(a: string, b: string) {
+function improveHandWithJoker(cards: { card: string; occurrences: number }[]) {
+  const jokerIndex = cards.findIndex((card) => card.card === "J");
+  if (jokerIndex !== -1) {
+    const joker = cards[jokerIndex];
+    if (joker.occurrences < 5) {
+      cards.splice(jokerIndex, 1);
+      cards[0].occurrences += joker.occurrences;
+    }
+  }
+  return cards;
+}
+
+function sortByWeakestHand(scale: Record<string, number>) {
+  return (a: HandInfo, b: HandInfo) => {
+    return handsCmp(b[0], a[0], scale);
+  };
+}
+
+function handsCmp(a: string, b: string, scale: Record<string, number>) {
   for (let i = 0; i < a.length; ++i) {
     if (a[i] !== b[i]) {
       return scale[b[i]] - scale[a[i]];
     }
   }
   return 0;
-}
-
-/* CHALLENGE 2 */
-export function solve2(input: string[]) {
-  const parsedInput = parseInput(input);
-  // console.log(parsedInput);
-  const {} = parseInput;
-
-  return 42;
-}
-
-/* SHARED */
-// Parse the input into a usable format. An example of input is provided below in the __forceInput.input variable.
-function parseInput(input: string[]) {
-  return input.map((line) => line.split(" ")).filter((line) => line.length > 1);
 }
 
 export const __forceInput = {
---
+++ utils/sorters.ts
@@ -0,0 +1,2 @@
+export const asc = (a: number, b: number) => a - b;
+export const desc = (a: number, b: number) => b - a;
