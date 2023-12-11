e66e1d1b693d93d0370a3782cd2f4b3093be4195
rstagi <r.stagi96@gmail.com>
2023-12-11 09:18:43 +0100

    fix: bootstrap day12

---
+++ day12/index.ts
@@ -0,0 +1,36 @@
+import * as __input from "../utils/input";
+import * as __reducers from "../utils/reducers";
+import * as __sorters from "../utils/sorters";
+import * as __math from "../utils/math";
+import * as __arrays from "../utils/arrays";
+
+/* CHALLENGE 1 */
+export function solve1(input: string[]) {
+  const parsedInput = parseInput(input);
+  console.log(parsedInput);
+  const {} = parsedInput;
+
+  return 42;
+}
+
+/* CHALLENGE 2 */
+export function solve2(input: string[]) {
+  const parsedInput = parseInput(input);
+  // console.log(parsedInput);
+  const {} = parsedInput;
+
+  return 42;
+}
+
+/* SHARED */
+// Parse the input into a usable format. An example of input is provided below in the __forceInput.input variable.
+function parseInput(input: string[]) {
+  return input;
+}
+
+export const __forceInput = {
+  force: true,
+  input: `
+
+`.trim(),
+};
